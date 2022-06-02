6999135e7107bb16d254da772811ee274285c047
Tusharkanti <tusharkantijoshi@gmail.com>
2022-06-02 14:21:43 +0530

    Checkpoint 2

---
+++ src/common/header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+import logo from './logo.svg';
+
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div className="header">
+            <img src={logo} alt="My logo" className="logo" />
+         </div>
+      );
+   }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Header from './common/header/Header.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <Header />
   </React.StrictMode>
 );
